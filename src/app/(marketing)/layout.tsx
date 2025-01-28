7183d23e7f78eb4fd608847ff2b09e68230fdf95
“Iris <liuirisny@gmail.com>
2025-01-28 09:59:54 -0500

    first commit

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from "react";
+
+export function MarketingLayout({children}: {
+    children: ReactNode}){
+        return <div className="selection:bg-[hsl(320,65%,52%,20%)]">
+            <NavBar />
+            {children}
+        </div>
+    
+}
